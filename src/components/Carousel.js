b5238e76394f3e376bd118a06df13d034ade20cf
paola piamba <paolapiamba57@gmail.com>
2022-11-15 14:40:41 -0300

    mi biografiaweb

---
+++ src/components/Carousel.js
@@ -0,0 +1,26 @@
+function Carousel() {
+    return (
+        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src="mami.jpg" className="img-fluid" alt="10" />
+                </div>
+                <div className="carousel-item">
+                    <img src="isa.jpg" className="img-fluid" alt="10" />
+                </div>
+                <div className="carousel-item">
+                    <img src="mi_abu.jpg" className="img-fluid" alt="10" />
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+    )
+}
+export default Carousel
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import Carousel from '../components/Carousel';
+
+function Home (){
+    return(
+        <div className="App">
+      <h1> ¡¡BIENVENIDOS!!<br />MI BIOGRAFIA </h1>
+      <p>Hola mi nombre es Paola Andrea Piamba,
+        tengo 21 años.
+        Tengo una hermosa bebe de 4 años,llamada Isabella.</p>
+      <Carousel />
+    </div>
+    )
+}
+export default Home
