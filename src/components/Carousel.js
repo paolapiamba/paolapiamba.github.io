ea35b2a1a4aa7105d05de6193d25319e29886c8e
paola piamba <paolapiamba57@gmail.com>
2022-11-16 12:15:17 -0300

    mi paginaweb paola

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -6,11 +6,19 @@ function Carousel() {
                     <img src="mami.jpg" className="img-fluid" alt="10" />
                 </div>
                 <div className="carousel-item">
-                    <p className="fst-italic">Esta es mi bebe Sara Isabella Castro piamba,de ella admiro que es una niña muy inteligente y educada.</p>
+                    <p className="fst-italic">Esta es mi bebe Sara Isabella Castro piamba,
+                        de ella admiro que es una niña muy inteligente y educada.</p>
+                    <br></br>
                     <img src="isa.jpg" className="img-fluid" alt="10" />
                 </div>
                 <div className="carousel-item">
-                    <p className="fst-item">Ella es mi motivacion para seguir estudiando y trabajando,y ahora estoy estudiando progracion  en la fundacion soy mas y esto es lo que he aprendido.html,css y javascrip.</p>
+                    <p className="fst-item">Ella es mi motivacion para seguir estudiando y trabajando.</p>
+                    <br></br>
+                    <a href="url" class="btn rounded-pill px-3 mb-2 mb-lg-0">Html,css y javascrip.</a>
+                    <br></br>
+                    Ahora estoy estudiando progracion  en la fundacion soy mas y esto es lo que he aprendido.
+                    <br></br>
+
                     <img src="thml.jpg" className="img-fluid" alt="10" />
                 </div>
             </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,15 @@ import Carousel from '../components/Carousel';
 
 function Home() {
     return (
-        <div className="App">
+        <div className="App container text-center">
             <h1> ¡¡BIENVENIDOS!!<br />MI BIOGRAFIA </h1>
+            <br></br>
             <p className='fst-italic'>Hola mi nombre es Paola Andrea Piamba,tengo 21 años.
+                <br></br>
                 Tengo una hermosa bebe de 4 años,llamada Isabella.</p>
+            <br></br>
+            <a href="https://www.aluracursos.com/blog/html-css-javascript-cuales-son-las-diferencias" class="btn rounded-pill px-3 mb-2 mb-lg-0">¿Qué es Html,css y javascript?</a>
+            <br></br>
             <Carousel />
         </div>
     )
