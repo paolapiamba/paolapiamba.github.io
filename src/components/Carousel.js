65841ebe765757e020fed80c9adf6f40c35238b6
paola piamba <paolapiamba57@gmail.com>
2022-11-22 13:45:50 -0300

    mi pagina

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -8,16 +8,16 @@ function Carousel() {
                 <div className="carousel-item">
                     <p className="fst-italic">Esta es mi bebe Sara Isabella Castro piamba,
                         de ella admiro que es una niña muy inteligente y educada.</p>
-                    <br></br>
+                    <br/>
                     <img src="isa.jpg" className="img-fluid" alt="10" />
                 </div>
                 <div className="carousel-item">
                     <p className="fst-item">Ella es mi motivacion para seguir estudiando y trabajando.</p>
-                    <br></br>
+                    <br/>
                     <a href="url" class="btn rounded-pill px-3 mb-2 mb-lg-0">Html,css y javascrip.</a>
-                    <br></br>
+                    <br/>
                     Ahora estoy estudiando progracion  en la fundacion soy mas y esto es lo que he aprendido.
-                    <br></br>
+                    <br/>
 
                     <img src="thml.jpg" className="img-fluid" alt="10" />
                 </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,13 +4,13 @@ function Home() {
     return (
         <div className="App container text-center">
             <h1> ¡¡BIENVENIDOS!!<br />MI BIOGRAFIA </h1>
-            <br></br>
+            <br/>
             <p className='fst-italic'>Hola mi nombre es Paola Andrea Piamba,tengo 21 años.
-                <br></br>
+                <br/>
                 Tengo una hermosa bebe de 4 años,llamada Isabella.</p>
             <br></br>
             <a href="https://www.aluracursos.com/blog/html-css-javascript-cuales-son-las-diferencias" class="btn rounded-pill px-3 mb-2 mb-lg-0">¿Qué es Html,css y javascript?</a>
-            <br></br>
+            <br/>
             <Carousel />
         </div>
     )
